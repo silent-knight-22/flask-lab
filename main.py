d7231efc57d9b2f21a422c8b27835f43501db93a
silent-knight-22 <36755496+silent-knight-22@users.noreply.github.com>
2025-11-12 18:56:15 +0530

    Update main.py

--- main.py
+++ main.py
@@ -6,4 +6,6 @@ def hello():
     return "Hello from Flask app deployed on Render!"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=10000)
+    import os
+    port = int(os.environ.get('PORT', 10000))
+    app.run(host='0.0.0.0', port=port)
